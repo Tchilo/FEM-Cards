777f19082767104c7f3d7675b50fbe8de71840c8
Tchilo <tchiloross@gmail.com>
2022-10-21 06:52:40 +0200

    Add expenses component to app

--- src/App.js
+++ src/App.js
@@ -5,12 +5,16 @@ import {
   Route,
 } from 'react-router-dom';
 import NftPreview from './Cards/NFT-preview-card/NFT-preview-card';
+import ExpensesChartComponent from './Cards/Expenses_chart_component/Expenses_chart_component';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path='nft' element={<NftPreview />} />
+        <Route path='Expenses_chart_component
+' element={<ExpensesChartComponent />} />
+
         {/* <Route path="teams" element={<Teams />}>
           <Route path=":teamId" element={<Team />} />
           <Route path="new" element={<NewTeamForm />} />
--- src/Cards/Expenses_chart_component/Expenses_chart_component.js
+++ src/Cards/Expenses_chart_component/Expenses_chart_component.js
@@ -1,1 +1,13 @@
-import './expenses_chart_component'
+import './expenses_chart_component.scss'
+
+import React from 'react'
+
+const ExpensesChartComponent = () => {
+  return (
+    <main>
+      
+      </main>
+  )
+}
+
+export default ExpensesChartComponent
