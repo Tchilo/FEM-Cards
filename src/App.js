78ad8c73a100b6944e94d99e92fd4851f6e6ee38
Tchilo <tchiloross@gmail.com>
2022-12-08 14:01:46 +0200

    update columns cards

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import ExpensesChartComponent from './Cards/Expenses_chart_component/Expenses_ch
 import { OrderSummaryComponent } from './Cards/Order-summary-component/Order-summary-component';
 import { ProfileCardComponent } from './Cards/Profile-card-component/Profile-card-component';
 import Product from './Cards/Product_preview_card_component/Product';
+import ColumnPreview from './Cards/column_preview_card_component/ColumnPreview';
 
 function App() {
   return (
@@ -20,6 +21,8 @@ function App() {
         <Route path='order-Summary' element={<OrderSummaryComponent />} />
         <Route path='profile-card-component' element={<ProfileCardComponent />} />
         <Route path='product-preview' element={<Product />} />
+        <Route path='3-columns' element={<ColumnPreview />} />
+
       </Routes>
     </BrowserRouter>
   )
